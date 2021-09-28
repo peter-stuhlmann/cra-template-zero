a5fd25ee696b9b2b5dc7cf34698ccfe91b363a76
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-09-29 00:06:28 +0200

    Import global stylesheet

--- template/src/App.js
+++ template/src/App.js
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import './assets/css/global.css';
+
 import Router from './components/Router';
 
 export default function App() {
